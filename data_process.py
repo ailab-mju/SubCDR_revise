66b2c16f2d77a51b406752816ee19cf562d4d855
ailab-mju <105773742+ailab-mju@users.noreply.github.com>
2023-12-10 21:33:10 +0900

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -5,7 +5,7 @@ from molFrags import *
 
 def data_process():
     #--------data_load
-    PATH = './SubCDR/data'
+    PATH = './SubCDR_revise/data'
     Drug_file = '%s/drug_smiles.csv'%PATH
     Cell_line_file = '%s/cell line_GEP.csv'%PATH
     Gene_role_file = '%s/gene_role.csv'%PATH
